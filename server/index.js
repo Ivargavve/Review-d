0ee1f14ffecbeecb9c4fe439a2f526fc1aa554fa
Ivar Gavelin <ivargavelin@gmail.com>
2024-04-02 01:43:42 +0200

    new mongoDB setup

--- server/index.js
+++ server/index.js
@@ -57,8 +57,6 @@ app.use('/posts', postRoutes);
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
 mongoose.connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
 }).then(() => {
     app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
