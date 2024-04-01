c3762dce789261b934694c6ce85c900c2f786caa
Ivar Gavelin <ivargavelin@gmail.com>
2024-04-02 00:01:46 +0200

    import for navbar
    a ton of import for the navbar

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -1,3 +1,30 @@
+import { useState } from 'react';
+import { 
+    Box,
+    IconButton,
+    InputBase,
+    Typgraphy,
+    Select,
+    MenuItem,
+    FormControl,
+    useTheme,
+    useMediaQuery
+ } from '@mui/material';
+import {
+    Search,
+    Message,
+    DarkMode,
+    LightMode,
+    Notifications,
+    Help,
+    Menu,
+    Close
+} from '@mui/icons-material';
+import { useDispatch, useSelector } from 'react-redux';
+import { setMode, setLogout } from 'state';
+import { useNavigate } from 'react-router-dom';
+import FlexBetween from 'components/flexBetween';
+
 const Navbar = () => {
     return (
         <div>
